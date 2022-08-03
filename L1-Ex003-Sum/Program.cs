c9b33d06b36518b4579c9087b71c05ef087389ec
Aeshe <syslichek@mail.ru>
2022-08-03 14:02:32 +0300

    add L1-Ex003-Sum

---
+++ L1-Ex003-Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int numberA = new Random().Next(0, 11);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(0, 11);
+Console.WriteLine(numberB);
+Console.Write("Сумма чисел равна: ");
+Console.WriteLine(numberA + numberB);
