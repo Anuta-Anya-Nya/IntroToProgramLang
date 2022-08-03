8e96d3a8877cfbc7cd467f9e88faeb85775e6199
Aeshe <syslichek@mail.ru>
2022-08-03 15:00:09 +0300

    add L1-Ex006-Max

---
+++ L1-Ex006-Max/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int numA = 2;
+int numB = 8;
+int numC = 4;
+int numD = 15;
+int numE = 0;
+int max = numA;
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+if (numD > max) max = numD;
+if (numE > max) max = numE;
+Console.Write("Максимальное число равно ");
+Console.WriteLine(max);
