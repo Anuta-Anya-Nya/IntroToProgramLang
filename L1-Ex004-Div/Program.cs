e0436e838946e7611e18e31595cc192e0bbd8aa3
Aeshe <syslichek@mail.ru>
2022-08-03 14:33:13 +0300

    add L1-Ex004-Div

---
+++ L1-Ex004-Div/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+double numberA = new Random().Next(0, 11);
+double numberB = new Random().Next(1, 11);
+Console.Write("Число 1: ");
+Console.WriteLine(numberA);
+Console.Write("Число 2: ");
+Console.WriteLine(numberB);
+Console.Write("Частное двух чисел равно: ");
+Console.WriteLine(numberA / numberB);
