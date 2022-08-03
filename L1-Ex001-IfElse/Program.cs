e9da1e46d51a7b9f7f05fc178fc88caa47071eaf
Aeshe <syslichek@mail.ru>
2022-08-03 14:49:50 +0300

    add L1-Ex001-IfElse

---
+++ L1-Ex001-IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Ваше имя: ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "маша") 
+{
+    Console.WriteLine("Ура! Это Маша!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
