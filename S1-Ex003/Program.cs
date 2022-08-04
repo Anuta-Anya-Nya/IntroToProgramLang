0a71cbceee5f6d4bb9b1dbce62203354360b5ead
Aeshe <syslichek@mail.ru>
2022-08-04 23:23:12 +0300

    Value validation

--- S1-Ex003/Program.cs
+++ S1-Ex003/Program.cs
@@ -1,10 +1,16 @@
-﻿Console. Write("Введите порядковый номер дня недели от 1 до 7: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number == 1) Console.Write("Понедельник");
-if (number == 2) Console.Write("Вторник");
-if (number == 3) Console.Write("Среда");
-if (number == 4) Console.Write("Четверг");
-if (number == 5) Console.Write("Пятница");
-if (number == 6) Console.Write("Суббота");
-if (number == 7) Console.Write("Воскресенье");
-if (number < 1 || number > 7) Console.WriteLine("Введено неверное число!");
+﻿Console.Write("Введите порядковый номер дня недели от 1 до 7: ");
+
+if (!int.TryParse(Console.ReadLine(), out int result))
+{
+    Console.WriteLine("Введено говно!");
+    return;
+}
+
+if (result == 1) Console.Write("Понедельник");
+if (result == 2) Console.Write("Вторник");
+if (result == 3) Console.Write("Среда");
+if (result == 4) Console.Write("Четверг");
+if (result == 5) Console.Write("Пятница");
+if (result == 6) Console.Write("Суббота");
+if (result == 7) Console.Write("Воскресенье");
+if (result < 1 || result > 7) Console.WriteLine("Введено неверное число");
