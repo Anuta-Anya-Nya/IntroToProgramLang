c14e9c9c0e2d8ab95633912ccf7518a7bf9804ea
Aeshe <syslichek@mail.ru>
2022-08-04 23:00:45 +0300

    Провели семинар

---
+++ S1-Ex002/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if ( num1 == num2 * num2 ) {
+    Console.Write("Да");
+} else {
+    Console.Write("Нет");
+}
+
---
+++ S1-Ex004/Program.cs
@@ -0,0 +1,8 @@
+﻿Console. Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count < number+1) 
+{
+    Console. WriteLine(count);
+    count++;
+}
